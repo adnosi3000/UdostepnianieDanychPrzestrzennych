f6ee69ec239c6d91134a5a772a1d195825898eff
adnosi3000 <>
2021-04-10 20:37:07 +0200

    dodano usługę WMTS BDOO BDOT10k

--- portalMapowyStronaGlownaWMTS.js
+++ portalMapowyStronaGlownaWMTS.js
@@ -31,9 +31,25 @@ var orto = L.tileLayer.projwmts('https://mapy.geoportal.gov.pl/wss/service/PZGIK
     attribution: 'Dostawca ortofotomapy &copy; <a href="https://www.geoportal.gov.pl/">Geoportal</a>'
 }).addTo(map);
 
+var topoGUGiK = L.tileLayer.projwmts('https://mapy.geoportal.gov.pl/wss/service/WMTS/guest/wmts/G2_MOBILE_500', {
+    format: 'image/png',
+    tileSize: 512,
+    version: '1.0.0',
+    transparent: true,
+    crs: crs,
+    origin: [850000.0, 100000.0],
+    scales: [3.0238155714285716E7, 1.5119077857142858E7, 7559538.928571429, 3779769.4642857146, 1889884.7321428573, 944942.3660714286, 472471.1830357143, 236235.59151785716, 94494.23660714286, 47247.11830357143, 23623.559151785714, 9449.423660714287, 4724.711830357143, 1889.8847321428573, 944.9423660714286, 472.4711830357143],
+    tilematrixSet: 'EPSG:2180',
+    opacity: 0.8,
+    crossOrigin: true,
+    minZoom: 5,
+    attribution: 'Dostawca ortofotomapy &copy; <a href="https://www.geoportal.gov.pl/">Geoportal</a>'
+}).addTo(map);
+
 var baseLayers = {
 "OSM": osm,
-"Ortofotomapa": orto
+"Ortofotomapa": orto,
+"BDOO / BDOT10k": topoGUGiK
 };
 
 L.control.layers(baseLayers).addTo(map);
